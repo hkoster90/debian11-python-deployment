4d2ebac76876009694102cc5af356ef8c4292e69
hkoster90 <72147107+hkoster90@users.noreply.github.com>
2022-08-09 20:13:30 +0100

    Update deploy_python.sh

--- deploy_python.sh
+++ deploy_python.sh
@@ -8,6 +8,10 @@ apt dist-upgrade -y
 
 apt install wget git curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 
+mkdir /tmp/debian11-python-deployment
+
+git clone https://github.com/nerdlertech/debian11-python-deployment.git /tmp/debian11-python-deployment
+
 wget wget https://www.python.org/ftp/python/3.10.5/Python-3.10.5.tgz
 tar -xf Python-3.10.*.tgz
 cd Python-3.10.*/
