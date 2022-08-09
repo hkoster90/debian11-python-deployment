c5c1ec1696ada9b0eee0f24a942d6f0e353d9134
hkoster90 <72147107+hkoster90@users.noreply.github.com>
2022-08-09 18:09:01 +0100

    Create deploy_python.sh

---
+++ deploy_python.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+
+apt update
+apt upgrade -y
+apt dist-upgrade -y
+
+apt install wget curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+
+wget wget https://www.python.org/ftp/python/3.10.5/Python-3.10.5.tgz
+tar -xf Python-3.10.*.tgz
+cd Python-3.10.*/
+./configure --enable-optimizations
+make -j $CORES
+make altinstall
+python3.10 --version
+cd ..
+pip install -r requirements.txt
