04e3853e2fec2fb148f6ff0e7668c9ae112bde27
hkoster90 <72147107+hkoster90@users.noreply.github.com>
2022-08-09 20:10:50 +0100

    Update deploy_python.sh

--- deploy_python.sh
+++ deploy_python.sh
@@ -6,7 +6,7 @@ apt update
 apt upgrade -y
 apt dist-upgrade -y
 
-apt install wget curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+apt install wget git curl build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 
 wget wget https://www.python.org/ftp/python/3.10.5/Python-3.10.5.tgz
 tar -xf Python-3.10.*.tgz
